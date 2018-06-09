accdbf79d3b44a483c91f65d1729a173e4a9ecb8
Kaung Myat Aung <kaungmyataung199@gmail.com>
2018-06-09 07:48:15 -0700

    recycler set up

---
+++ app/src/main/java/com/padcmyanmar/ted2assignment/adapters/TedTalkAdapter.java
@@ -0,0 +1,35 @@
+package com.padcmyanmar.ted2assignment.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.padcmyanmar.ted2assignment.R;
+import com.padcmyanmar.ted2assignment.viewHolders.TedTalksViewHolder;
+
+public class TedTalkAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.view_holder_ted, parent, false);
+
+        return new TedTalksViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+}
---
+++ app/src/main/java/com/padcmyanmar/ted2assignment/viewHolders/TedTalksViewHolder.java
@@ -0,0 +1,10 @@
+package com.padcmyanmar.ted2assignment.viewHolders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class TedTalksViewHolder extends RecyclerView.ViewHolder {
+    public TedTalksViewHolder(View itemView) {
+        super(itemView);
+    }
+}
