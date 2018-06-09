39cd42cdd5736274ea0e67bbccd9eb648ff640eb
Kaung Myat Aung <kaungmyataung199@gmail.com>
2018-06-09 08:16:36 -0700

    string

---
+++ app/src/main/java/com/padcmyanmar/ted2assignment/delegates/TedDelegate.java
@@ -0,0 +1,9 @@
+package com.padcmyanmar.ted2assignment.delegates;
+
+public interface TedDelegate {
+    void onTapNews();
+    void onTapfavourite();
+    void OnTapCommend();
+    void OnTapSentTo();
+    void onTapStatistics();
+}
